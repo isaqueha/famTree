05b8a8518e04bd838fc8fc52de5b76b01f2e368c
Isaque Azevedo <isaque.azevedo@sap.com>
2019-07-12 00:13:32 -0300

    Adding TS + D3 project

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Family Trees in TS and D3
 
 ### D3
 https://spin.atomicobject.com/2017/07/20/d3-react-typescript/
-
+https://github.com/korydondzila/React-TypeScript-D3
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './components/App';
+import './styles/index.css';
+import data from "./data";
+
+ReactDOM.render(
+  <App
+    width={window.screen.availWidth}
+    height={window.screen.availHeight}
+    graph={data} />,
+  document.getElementById('root')
+);
